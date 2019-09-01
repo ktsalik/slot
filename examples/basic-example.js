591e7bfe5d80b57fb973af4c5117c93580065efd
Kostas Tsalikidis <tsalikidhs@gmail.com>
2019-09-01 10:22:29 +0300

    load resources method

--- examples/basic-example.js
+++ examples/basic-example.js
@@ -1,4 +1,17 @@
 var game = new Slot({
   container: '#game-container',
 });
-// document.body.innerHTML += `Slot v${game.version}`;
+
+game.load([
+  ['symbol-1', 'assets/space adventure/symbol-1.png'],
+  ['symbol-2', 'assets/space adventure/symbol-2.png'],
+  ['symbol-3', 'assets/space adventure/symbol-3.png'],
+  ['symbol-4', 'assets/space adventure/symbol-4.png'],
+  ['symbol-5', 'assets/space adventure/symbol-5.png'],
+  ['symbol-6', 'assets/space adventure/symbol-6.png'],
+  ['symbol-7', 'assets/space adventure/symbol-7.png'],
+  ['symbol-8', 'assets/space adventure/symbol-8.png'],
+  ['symbol-9', 'assets/space adventure/symbol-9.png'],
+  ['symbol-10', 'assets/space adventure/symbol-10.png'],
+], function() {
+});
--- src/slot.js
+++ src/slot.js
@@ -15,3 +15,14 @@ var Slot = function(params) {
     document.body.appendChild(this.engine.renderer.view);
   }
 };
+
+Slot.prototype.load = function(config, onComplete) {
+  config.forEach(function(resource) {
+    if (resource.length) {
+      PIXI.Loader.shared.add(resource[0], resource[1]);
+    } else {
+      PIXI.Loader.shared.add(resource.key, resource.value);
+    }
+  });
+  PIXI.Loader.shared.load(onComplete);
+};
