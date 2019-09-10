46501f4725c1fe50fb6214063658bac359445066
Kostas Tsalikidis <tsalikidhs@gmail.com>
2019-09-10 08:56:36 +0300

    coordinates to sprites

--- src/sprite.js
+++ src/sprite.js
@@ -1,11 +1,15 @@
 Slot.Sprite = function(resourceKey, engine) {
   this.instance = new PIXI.Sprite(PIXI.Loader.shared.resources[resourceKey].texture);
+  this.x = 0;
+  this.y = 0;
 
   var _this = this;
   function resizeAndPosition() {
     var pixiSprite = _this.instance;
     pixiSprite.scale.x = engine.renderer.view.width / engine.width;
     pixiSprite.scale.y = engine.renderer.view.height / engine.height;
+    pixiSprite.x = (_this.x * engine.width) / engine.renderer.view.width;
+    pixiSprite.y = (_this.y * engine.height) / engine.renderer.view.height;
   }
   resizeAndPosition();
   PIXI.Ticker.shared.add(function() {
